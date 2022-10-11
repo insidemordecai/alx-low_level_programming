33aac6484f2074b2e1b6d8d40e60aab14cc020c2
Mordecai K <mordecai.kipngetich@gmail.com>
2022-10-11 02:39:41 -0700

    fix: 3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -4,9 +4,10 @@
 /**
  * File: 3-function_like_macro.h
  * a function-like macro ABS(x)
- * that computes the absolute value of a number x.
+ * that computes the absolute value of a number x
+ * @x: number to be computed
  */
 
-#define ABS(x) ((x) < 0 ? (-x) : (x))
+#define ABS(x) ((x) < 0 ? -(x) : (x))
 
 #endif
