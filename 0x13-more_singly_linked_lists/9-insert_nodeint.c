a18c62d203725911429e4b3bd2ee4a91db9e820e
Mordecai K <mordecai.kipngetich@gmail.com>
2022-10-26 13:46:22 +0300

    update: remove trailing whitespaces

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -34,7 +34,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
             temp->next = new;
             return (new);
         }
-        
+
 		temp = temp->next;
     }
 
