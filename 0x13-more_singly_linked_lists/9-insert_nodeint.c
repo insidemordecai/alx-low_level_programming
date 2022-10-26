192d6785e15935ed09720c66c9e7de6004745ca5
Mordecai K <mordecai.kipngetich@gmail.com>
2022-10-26 13:47:58 +0300

    fix: indent issue

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,34 +9,34 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-    unsigned int i;
-    listint_t *new, *temp = *head;
+	unsigned int i;
+	listint_t *new, *temp = *head;
 
-    new = malloc(sizeof(listint_t));
+	new = malloc(sizeof(listint_t));
 
-    if (new == NULL || head == NULL)
-        return (NULL);
+	if (new == NULL || head == NULL)
+		return (NULL);
 
-    new->n = n;
+	new->n = n;
 
-    if (idx == 0)
-    {
-        new->next = *head;
-        *head = new;
-        return (new);
-    }
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
 
-    for (i = 0; temp && i < idx; i++)
-    {
-        if (i == idx - 1)
-        {
-            new->next = temp->next;
-            temp->next = new;
-            return (new);
-        }
+	for (i = 0; temp && i < idx; i++)
+	{
+		if (i == idx - 1)
+		{
+			new->next = temp->next;
+			temp->next = new;
+			return (new);
+		}
 
 		temp = temp->next;
-    }
+	}
 
-    return (NULL);
+	return (NULL);
 }
