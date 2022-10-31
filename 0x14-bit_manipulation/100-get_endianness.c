3b67b2a6e3a10bf5233124b0ca3fbc1cbf479d1d
Mordecai K <mordecai.kipngetich@gmail.com>
2022-10-31 14:04:45 +0300

    add: advanced tasks

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness
+ * Return: 0 if big endian, else 1 (little endian)
+ */
+int get_endianness(void)
+{
+	unsigned int i = 1;
+    char *endian = (char *)&i;
+
+    return (*endian);
+}
