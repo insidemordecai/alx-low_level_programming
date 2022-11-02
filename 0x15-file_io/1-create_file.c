8b2a670a050d4a456d4ef3791b63cff71c898efc
Mordecai K <mordecai.kipngetich@gmail.com>
2022-11-02 20:43:41 +0300

    fix: had 0 instead of o

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(0, text_content, length);
+	w = write(o, text_content, length);
 
 	if (o == -1 || w == -1)
 		return (-1);
